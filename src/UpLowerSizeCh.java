e45c2d838f791b7b68b6ab52935167c4e9e95eea
왕준수 <wnstn819@douzone.com>
2022-08-04 22:44:22 +0900

    Initial commit

---
+++ src/UpLowerSizeCh.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class UpLowerSizeCh{
+    public int solution(String str, char t){
+        int answer=0;
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        UpLowerSizeCh T = new UpLowerSizeCh();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("iiihi");
+
+    }
+}
