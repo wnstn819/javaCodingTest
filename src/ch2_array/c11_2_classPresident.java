f641ece13a915f89588d5e783a679c5161666f25
wnstn819 <wnstn819@naver.com>
2022-08-22 15:38:33 +0900

    Array11 - 혼자서 못품.. 다시 풀어보자(2)

--- src/ch2_array/c11_2_classPresident.java
+++ src/ch2_array/c11_2_classPresident.java
@@ -11,8 +11,8 @@ public class c11_2_classPresident {
         int n = sc.nextInt();
         int[][] array = new int[n + 1][6];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < 5; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
 
                 array[i][j] = sc.nextInt();
 
@@ -29,8 +29,25 @@ public class c11_2_classPresident {
     }
 
     public int solution(int n, int[][] arr) {
-        System.out.println(arr.length);
-        return n;
+        int answer = 0, max = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+        return answer;
 
     }
 }
