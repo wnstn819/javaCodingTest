edc38758978b392e26a6c7ee0a8ac339021c2992
wnstn819 <wnstn819@naver.com>
2022-11-10 15:08:08 +0900

    S5 - 2026 종이 자르기 완료 S5

--- src/solved/S/S5_2628/Main.java
+++ src/solved/S/S5_2628/Main.java
@@ -2,6 +2,9 @@ package solved.S.S5_2628;///////////////////////////////////////////////////////
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,51 +14,49 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-
+        List<Integer> wList = new LinkedList<>();
+        List<Integer> hList = new LinkedList<>();
         st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
-
-        int[][] line = new int[T][2];
-        int TCount = 0;
-        int LCount = 0;
-        for (int i = 0; i < T; i++) {
+        for (int testcase = 0; testcase < T; testcase++) {
             st = new StringTokenizer(br.readLine());
-
             if (Integer.parseInt(st.nextToken()) == 0) {
-                LCount++;
-                line[i][0] = Integer.parseInt(st.nextToken());
+                wList.add(Integer.parseInt(st.nextToken()));
             } else {
-                line[0][i] = Integer.parseInt(st.nextToken());
-                TCount++;
+                hList.add(Integer.parseInt(st.nextToken()));
             }
 
 
         }
-        int[][] arr = new int[m + LCount][n + TCount];
-        for (int testCase = 0; testCase < T; testCase++) {
+        wList.add(0);
+        wList.add(m);
+        hList.add(0);
+        hList.add(n);
 
-            if (line[testCase][0] == 0) {
-                System.out.println(line[0][testCase]);
-                for (int i = 0; i < n + TCount - 1; i++) {
-                    arr[i][line[0][testCase]] += 1;
-                }
-            } else {
-                for (int i = 0; i < m + LCount + 1; i++) {
-                    arr[line[testCase][0]][i] += 1;
-                }
+        Collections.sort(wList);
+        Collections.sort(hList);
+
+        int wMax = 1;
+        int hMax = 1;
+        for (int i = 1; i < wList.size(); i++) {
+            if (wList.get(i) - wList.get(i - 1) > wMax) {
+                wMax = wList.get(i) - wList.get(i - 1);
+            }
+        }
+
+        for (int i = 1; i < hList.size(); i++) {
+            if (hList.get(i) - hList.get(i - 1) > hMax) {
+                hMax = hList.get(i) - hList.get(i - 1);
             }
 
         }
 
-        for (int i = 0; i < m + LCount; i++) {
-            for (int j = 0; j < n + TCount; j++) {
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
+        sb.append(hMax * wMax);
+        System.out.println(sb);
 
 
     }
