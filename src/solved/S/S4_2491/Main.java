61bc7a879576a43316b3a8adf71943aa6c5e282d
wnstn819 <wnstn819@naver.com>
2022-11-23 18:15:45 +0900

    S4_2491 수열

--- src/solved/S/S4_2491/Main.java
+++ src/solved/S/S4_2491/Main.java
@@ -2,26 +2,38 @@ package solved.S.S4_2491;///////////////////////////////////////////////////////
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer> resultList = new LinkedList<>();
-
-
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(st.nextToken());
-
-
-        sb.append(resultList.size() + "\n");
-        for (int i = 0; i < resultList.size(); i++) {
-            sb.append(resultList.get(i) + " ");
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[T];
+        for (int i = 0; i < T; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        int max = 1;
+        int count = 1;
+        for (int i = 0; i < T - 1; i++) {
+            if (arr[i] <= arr[i + 1]) count++;
+            else count = 1;
+            max = Math.max(max, count);
+        }
+
+        int count2 = 1;
+        for (int i = 0; i < T - 1; i++) {
+            if (arr[i] >= arr[i + 1]) count2++;
+            else count2 = 1;
+            max = Math.max(max, count2);
+        }
+
+
+        sb.append(max + "\n");
+
         System.out.println(sb);
 
 
