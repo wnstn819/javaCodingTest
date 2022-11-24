8c531fc36dcdada54b2c214a7b3323872b24ff85
wnstn819 <wnstn819@naver.com>
2022-11-24 19:11:44 +0900

    B1_2596 비밀편지

---
+++ src/solved/B/B1_2596/Main.java
@@ -0,0 +1,22 @@
+package solved.B.B1_2596;/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int testCase = 0; testCase < 9; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        }
+
+
+    }
+}
+
+
