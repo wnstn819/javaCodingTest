0a553aa010e8b5109b0c69c47dcbaa79124e4f74
wnstn819 <wnstn819@naver.com>
2022-12-11 16:53:20 +0900

    p.lv1.hambugger

---
+++ src/programmers/lv1/Solution.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        return answer;
+    }
+}
---
+++ src/programmers/lv1/hambugger/Solution.java
@@ -0,0 +1,10 @@
+package programmers.lv1.hambugger;
+
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        return answer;
+    }
+}
