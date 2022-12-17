28010814c5c65a4428a981b0990790b8d6434a8e
wnstn819 <wnstn819@naver.com>
2022-12-17 20:22:45 +0900

    solved.G.G3_17298 오큰수 구하기

---
+++ src/solved/G/G3_17298_doit/Main.java
@@ -0,0 +1,41 @@
+//오큰수 구하기
+package solved.G.G3_17298_doit;
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(st.nextToken());
+        int[] A = new int[m];
+        int[] ans = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 1; i < m; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                ans[stack.pop()] = A[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            ans[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            sb.append(ans[i] + " ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
