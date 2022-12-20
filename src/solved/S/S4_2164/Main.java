fdfe2e3aacef2396a21b98d6d1fe7f8259e862e4
wnstn819 <wnstn819@naver.com>
2022-12-20 17:34:28 +0900

    S4_2164_doit 카드게임

--- src/solved/S/S4_2164/Main.java
+++ src/solved/S/S4_2164/Main.java
@@ -1,3 +1,4 @@
+//카드 게임
 package solved.S.S4_2164;
 
 import java.io.*;
@@ -8,6 +9,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        Queue<Integer> que = new LinkedList<>();
+
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= m; i++) {
+            que.add(i);
+        }
+
+        while (que.size() > 1) {
+            que.poll();
+            que.add(que.poll());
+        }
+
+        sb.append(que.poll());
+        System.out.println(sb);
 
     }
 }
