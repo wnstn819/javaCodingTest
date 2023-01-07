74872667b73ba0e7d3346be63765c0041bd5a504
wnstn819 <wnstn819@naver.com>
2023-01-07 18:06:24 +0900

    S1_1913_doit - 회의실 배정하기

---
+++ src/solved/S/S1/S1_1913_doit/Main.java
@@ -0,0 +1,44 @@
+//회의실 배정하기 - 배열 세부 정렬
+
+package solved.S.S1.S1_1913_doit;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i][0] >= end) {
+                end = arr[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+    }
+}
